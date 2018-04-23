a5c5a5e0253bac0c9d8b49f2896b8f77f4a9c4e5
16014753 <16014753@myrp.edu.sg>
2018-04-23 11:48:44 +0800

    Rest of the Images

--- app/src/main/java/com/myapplicationdev/android/demoimageview/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoimageview/MainActivity.java
@@ -7,12 +7,15 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     ImageView ivDay2;
+    ImageView ivDay4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ivDay2 = findViewById(R.id.imageView2);
+        ivDay4 = findViewById(R.id.imageView3);
         ivDay2.setImageResource(R.drawable.day2);
+        ivDay4.setImageResource(R.drawable.day4);
     }
 }
